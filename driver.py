ac1899ed3ea2fe04485acc0b7ae53f78e2aacf90
stevebetch <stevebetch@dce78607-3b2a-a8d0-072d-236a953dce24>
2014-04-02 03:58:49 +0000

    #! /usr/bin/env python #file: driver.py #Author: Stephan Boettcher #Desc:This program is the driver function for the parallel form of the drone simulation. #Program creates the drone, the 3d map space needed for the sim, the targets and controls everything #uses the parallel Python software
    import sys
    import time
    import thread
    import sys
    import pp
    import func/map

---
+++ driver.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+#file: driver.py
+#Author: Stephan Boettcher
+#Desc:This program is the driver function for the parallel form of the drone simulation.
+#Program creates the drone, the 3d map space needed for the sim, the targets and controls everything
+#uses the parallel Python software
+
+
+import sys
+import time
+import thread
+import sys
+import pp
+import func/map
+
