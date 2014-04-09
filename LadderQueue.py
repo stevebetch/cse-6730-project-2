3cc1cdb2ff21922c5113cff794e6ed08eae03692
keith.andrew.j <keith.andrew.j@gmail.com>
2014-04-09 01:54:10 +0000

    Significant updates but still not complete enough to run

--- LadderQueue.py
+++ LadderQueue.py
@@ -1,51 +1,87 @@
 import sys
+from Message import * 
 
 class LadderQueue:
     
     def __init__(self):
-        pass
+        pass 
 
-    def enqueue():
-	if TS>=TopStart:
-		# insert into tail of Top
-		NTop+=1
-		return
-	while (TS<RCur[x] and x <= NRung):
-		x+=1
-		if x<=NRung:
-			bucket_k=(TS - Rstart[x])/Bucketwidth[x] # - or --?
-			# insert into tail of rung x, bucket_k
-			NBucket[x, bucket_k]+=1
+	MaxTS=0
+	MinTS=999999
+	x=0
+	k=0
+	NTop=0
+	TopStart=0
+	Bucketwidth=[]
+	NBc=0
+	NBucket=[]
+	bucket_k=0
+	NRung=0
+	RCur=[]
+	RStart=[]
+	THRES=50
+	NBot=0
+	Top=[]
+	Bottom=[]
+
+	def enqueue(message):
+		if TS>=TopStart:
+			# insert into tail of Top
+			Top.insert(len(Top),message)
+			NTop+=1
+			if message.timestamp>MaxTS:
+				MaxTS=message.timestamp
+			if message.timestamp<MinTS:
+				MinTS=message.timestamp
 			return
-		elif NBot>THRES:
-			create_new_rung(NBot)
-			# transfer Bottom to it
-			bucket_k=(TS-RStart[NRung])/Bucketwidth(NRung) # - or --?
-			# insert into tail of rung NRung, bucket_k
-			NBucket[NRung, bucket_k]+=1
-		else:
-			# insert into Bottom using sequential search
-			Nbot+=1
+		while (TS<RCur[x] and x <= NRung):
+			x+=1
+			if x<=NRung:
+				bucket_k=(TS - Rstart[x])/Bucketwidth[x] # - or --?
+				# insert into tail of rung x, bucket_k
+				NBucket[x, bucket_k]+=1
+				return
+			elif NBot>THRES:
+				create_new_rung(NBot)
+				# transfer Bottom to it
+				bucket_k=(TS-RStart[NRung])/Bucketwidth(NRung) # - or --?
+				# insert into tail of rung NRung, bucket_k
+				NBucket[NRung, bucket_k]+=1
+			else:
+				# insert into Bottom using sequential search
+				i=0
+				while i<len(Bottom):
+					if message.timestamp<Bottom(i):
+						Bottom.insert(i,message)
+						break
+					i+=1
+				Nbot+=1
 
 
 	def dequeue():
-		if 1==1: # replace with Bottom not empty
-			# return next event from Bottom
-		    pass
+		if len(Bottom)!=0:
+			# return next message from Bottom
+			Bottom.pop(0)
+			return
 		if NRung>0:
 			bucket_k=recurse_rung()
 			# if last bucket then NRung- #not sure what the - means?
 			# sort from bucket_k to Bottom
-			# return first event from Bottom
+			# return first message from Bottom
+			Bottom.pop(0)
 		else:
-			Bucketwidth[1]=(MaxTS-MinTS)/NTop # - or --?
-			TopStart=MaxTS
-			RCur[1]=MinTS # check swap order a
-			Rstart[1]=RCur[1] # check swap order b
+			Bucketwidth[1]=(MaxTS-MinTS)/NTop
+			Bucketwidth[0]=Bucketwidth[1]
+			TopStart=MaxTS+Bucketwidth[1] #p 179 disagrees with pseudo code, check
+			RCur[1]=MinTS
+			RStart[1]=MinTS
 			# transfer Top to rung 1 of Ladder
+			bucket_k=floor((TS-RStart[1])/Bucketwidth[1])
+
 			bucket_k=recurse_rung()
-			# sort events from bucket_k and copy to Bottom
-			# return first event from Bottom
+			# sort messages from bucket_k and copy to Bottom
+			# return first message from Bottom
+			return Bottom.pop(0)
 
 
 	def recurseRung():
@@ -54,15 +90,16 @@ class LadderQueue:
 			k+=1
 			RCur[NRung]+=Bucketwidth[NRung]
 		if NBucket[NRung, k]>THRES:
-			create_new_rung(NBucket[NRung,k])
-			# recopy events from bucket to new rung
+			createNewRung(NBucket[NRung,k])
+			# recopy messages from bucket to new rung
 			# goto Point A
 		return k
 
 
 	def createNewRung():
 		NRung+=1
-		Bucketwidth[NRung]=Bucketwidth[NRung-1]/NEvent
-		RCur[NRung]=RCur[NRung-1] # check swap order c
-		RStart[NRung]=RCur[NRung] # check swap order d
+		Bucketwidth[NRung]=Bucketwidth[NRung-1]/THRES
+		RCur[NRung]=RCur[NRung-1] 
+		RStart[NRung]=RCur[NRung-1]
+		#11RCur[NRung-1]+=Bucketwidth[NRung-1]
     
