d6b838ec002bdf245c3779240acd62df6c0ba3e8
keith.andrew.j <keith.andrew.j@gmail.com>
2014-04-11 11:23:57 +0000

    Implementation of "Naive" heuristic (still needs debugging).
    Primary issue: everything I debug for this file happens twice - not sure why

--- CAOC.py
+++ CAOC.py
@@ -20,7 +20,7 @@ class CAOC (LogicalProcess):
         self.priorityQueue = Queue()
         self.drones=[]
         for i in range(0,numDrones):
-            self.drones.insert(i,[0,0])
+            self.drones.insert(i,["Idle",0])
         
     def __call__(self):
         self.run()
@@ -32,10 +32,21 @@ class CAOC (LogicalProcess):
         self.hmint = hmint
         
     def addTarget(self, target):
-        priority = self.getPriority(target)
-        #self.priorityQueue.put(priority, target)
-        self.priorityQueue.put(target)
-        print('CAOC Added target to priority queue')
+        switch=0
+        for i in range(0,len(self.drones)):
+            if self.drones[i][0]=="Idle":
+                # send message to drone LP
+                switch=1
+                tgtData=[target.data[0],target.data[1],target.data[2],target.data[3],target.data[4],target.data[5],target.data[6],target.data[7],target.data[9]]
+                newTgt=Message(getNextMessageID(),2,tgtData,self.id,i,t_Now)
+                # SEND MESSAGE - how to do this?
+                break
+        if switch==0:    
+            priority = self.getPriority(target)
+            #self.priorityQueue.put(priority, target)
+            self.priorityQueue.put(target)
+            print('CAOC Added target to priority queue')
+
     
     def getPriority(self, target):
         # generate priority
@@ -60,6 +71,9 @@ class CAOC (LogicalProcess):
                 self.addTarget(msg)
             elif msg.msgType==3:
                 self.drones[msg.data[0]]=[msg.data[1],msg.data[2]]
+                if self.drones[msg.data[0]][1]=="Idle":
+                    #tgtData=priorityQueue.pop(0)
+                    newTgt=Message(getNextMessageID(),2,tgtData,self.id,msg.data[0],t_Now)
             pass    
 
     def run(self):
@@ -69,7 +83,9 @@ class CAOC (LogicalProcess):
         
 
 # DEBUGGING
-status_data=[3,"Idle",13]
+status_data=[1,"Busy",13]
 status_msg=Message(1,3,status_data,5,1,11)
+print status_msg.data[0]
 c=CAOC(2)
+c.handleMessage(status_msg)
 print c.drones
