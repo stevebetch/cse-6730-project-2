5e652d2749fc1b99fe1f1e65294ff20e44380f3d
keith.andrew.j <keith.andrew.j@gmail.com>
2014-04-19 18:57:52 +0000

    Debug test file for save and restore methods

---
+++ TestSaveRestoreState.py
@@ -0,0 +1,63 @@
+from random import *
+from multiprocessing import Queue, Lock
+from Map import *
+from nodes import *
+from Target import *
+from CAOC import *
+from HMINT import *
+from Drone import *
+from IMINT import *
+from LogicalProcess import *
+from LPInputQueue import *
+from state import *
+
+
+def main():
+    print 'Start'
+    
+    mapX=300
+    mapY=300
+    numStreets=50
+    numTargets=10
+    numDrones=3
+    heuristic=1
+    seedNum=1
+    
+    print '---Generate Map---'
+    Map = GenMap(mapX,mapY)
+    Map.map(numStreets)
+    randNodes=[]
+    for i in range(numTargets):
+        randNodes.append(Map.RandNode())
+ 
+    print '---Generate CAOC and IMINT---'
+    caoc=CAOC(numDrones,heuristic)
+    hmint = HMINT(numTargets, seedNum, randNodes)
+    caoc.setHMINT(hmint)
+    hmint.setCAOC(caoc)
+    caocInQ = LPInputQueue()
+    
+    imint=IMINT(heuristic)
+    imintInQ = LPInputQueue()
+    print 'IMINT total value: ' + str(imint.totalValue)
+
+    
+    print '---Generate and Add Messages---'
+    m2Data=[0,80,80,'Vehicle',1,1,randNodes[0],30,0,0]
+    m2=Message(2,m2Data,'CAOC','IMINT',0)
+    m3Data=[0,'Idle',randNodes[1]]
+    m3=Message(3,m3Data,'IMINT','CAOC',1)    
+    
+    print '---Save and Restore State---'
+    print 'CAOC Local Time: ' + str(caoc.localTime)
+    print 'IMINT Local Time: ' + str(imint.localTime)
+    caoc.saveState()
+    print 'CAOC state queue: ' + str(caoc.stateQueue)    
+    imint.saveState()
+    print 'IMINT state queue: ' + str(imint.stateQueue)
+
+    #caoc.restoreState(0)
+    #imint.restoreState(0)
+    print 'End'
+
+main()
